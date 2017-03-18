a5de55def0184b99318f6d8336f266aa3101d455
Gamaliel Petralba <petralbagamaliel@gmail.com>
2017-03-18 21:06:22 +0800

    Minor edits.

--- Summarizing and Presenting Data/src/main/Main.java
+++ Summarizing and Presenting Data/src/main/Main.java
@@ -2,11 +2,9 @@ package main;
 	
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
-import javafx.stage.Stage;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.layout.BorderPane;
-
+import javafx.stage.Stage;
 
 public class Main extends Application {
 	@Override
@@ -22,7 +20,7 @@ public class Main extends Application {
 			e.printStackTrace();
 		}
 	}
-	
+
 	public static void main(String[] args) {
 		launch(args);
 	}
