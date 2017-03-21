29cde17697e17d5b2bf0f793e38e91d5673465dd
Enrico <jerome.requioma@gmail.com>
2017-03-21 16:59:16 +0800

    edited some parts of the pie chart

--- Summarizing and Presenting Data/src/main/PieChartController.java
+++ Summarizing and Presenting Data/src/main/PieChartController.java
@@ -6,11 +6,15 @@ import java.util.ResourceBundle;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.geometry.Side;
 import javafx.scene.chart.PieChart;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 
 public class PieChartController implements Initializable{
@@ -20,14 +24,29 @@ public class PieChartController implements Initializable{
 	
 	@Override
 	public void initialize(URL arg0, ResourceBundle arg1) {
-		for (int i = 0; i < MainFields.getSampleData().size(); i++) {
-			details.add(new PieChart.Data(MainFields.getSampleData().get(i), 1));
+		for (int i = 0; i < MainFields.getFusedData().size(); i++) {
+			details.add(new PieChart.Data(MainFields.getFusedData().get(i), MainFields.getDataPercentage().get(i)));
 		}
 		
 		pieChart.setData(details);
 		pieChart.setTitle(MainFields.getTitle());
 		pieChart.setLegendSide(Side.BOTTOM);
 		pieChart.setLabelsVisible(true);
+		
+		final Label caption = new Label("");
+		caption.setTextFill(Color.DARKORANGE);
+		caption.setStyle("-fx-font: 24 arial;");
+
+		for (final PieChart.Data data : pieChart.getData()) {
+		    data.getNode().addEventHandler(MouseEvent.MOUSE_PRESSED,
+		        new EventHandler<MouseEvent>() {
+		            @Override public void handle(MouseEvent e) {
+		                caption.setTranslateX(e.getSceneX());
+		                caption.setTranslateY(e.getSceneY());
+		                caption.setText(String.valueOf(data.getPieValue()) + "%");
+		             }
+		        });
+		}
 		
 	}
 	
