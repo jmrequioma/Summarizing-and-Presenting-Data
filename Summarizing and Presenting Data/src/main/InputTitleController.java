2cb3a58f33f3f5f3e067946d03392eb40489e0d3
Enrico <jerome.requioma@gmail.com>
2017-03-23 22:59:48 +0800

    empty table title is not allowed

--- Summarizing and Presenting Data/src/main/InputTitleController.java
+++ Summarizing and Presenting Data/src/main/InputTitleController.java
@@ -7,7 +7,10 @@ import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.Button;
+import javafx.scene.control.DialogPane;
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 
@@ -19,17 +22,25 @@ public class InputTitleController {
 	@FXML
 	private void confirm(ActionEvent event) throws IOException {
 		Stage stage = (Stage) btnBack.getScene().getWindow();
-		stage.close();
 		
 		String title = txtFInputTitle.getText();
-		MainFields.setTitle(title);
-		
-		Stage ownerStage = (Stage) stage.getOwner();
-		Parent root = FXMLLoader.load(getClass().getResource("DataPresentation.fxml"));
-		Scene scene = new Scene(root);
-		scene.getStylesheets().add("/main/bloodcrimson.css");
-		ownerStage.setScene(scene);
-		ownerStage.show();
+		if (title.equals("")) {
+			Alert alert = new Alert(AlertType.ERROR);
+			alert.setTitle("Critical Error");
+			alert.setHeaderText("Invalid Input!!!");
+			alert.setContentText("Ooops, input is not allowed! Please change the input.");
+			alert.showAndWait();
+		} else {
+			MainFields.setTitle(title);
+			
+			Stage ownerStage = (Stage) stage.getOwner();
+			Parent root = FXMLLoader.load(getClass().getResource("DataPresentation.fxml"));
+			Scene scene = new Scene(root);
+			scene.getStylesheets().add("/main/bloodcrimson.css");
+			ownerStage.setScene(scene);
+			ownerStage.show();
+			stage.close();
+		}
 	}
 	
 	@FXML
