757222f678d348e7c130e03c4f6fe00d3366fe90
Gamaliel Petralba <petralbagamaliel@gmail.com>
2017-03-23 18:54:46 +0800

    Numerical table. unfinished

--- Summarizing and Presenting Data/src/main/NumericalTableController.java
+++ Summarizing and Presenting Data/src/main/NumericalTableController.java
@@ -26,12 +26,17 @@ public class NumericalTableController implements Initializable {
 	public void initialize(URL arg0, ResourceBundle arg1) {
 		ArrayList<Float> sampleDataFloat = MainFields.getSampleDataFloat();
 		Collections.sort(sampleDataFloat);
+		printSampleDataFloat(sampleDataFloat);
+		int mostDecimals = mostDecimals(sampleDataFloat);
 		
-//		float highestVal = sampleDataFloat.get(sampleDataFloat.size() - 1);
-//		float lowestVal = sampleDataFloat.get(0);
-//		float range = highestVal - lowestVal;
-//		int numClasses = sturge(range);
-//		float width = range / numClasses;
+		float highestVal = sampleDataFloat.get(sampleDataFloat.size() - 1);
+		float lowestVal = sampleDataFloat.get(0);
+		float range = highestVal - lowestVal;
+		int numClasses = sturge(sampleDataFloat.size());
+		float width = range / numClasses;
+		width = roundUp(width, mostDecimals);
+		System.out.println("Number of classes: " + numClasses); // test
+		System.out.println("Width: " + width); // test
 //		ArrayList<String> classLimitList = classLimit(lowestVal, numClasses, width);
 //		ArrayList<String> trueClassLimitList = trueClassLimit(lowestVal, numClasses, width);
 //		ArrayList<Float> midpointList = midpoint(lowestVal, numClasses, width);
@@ -56,15 +61,82 @@ public class NumericalTableController implements Initializable {
 				<NumericalData, Float>("cumulativePercentage"));
 	}
 	
+	// debugging purposes
+	private void printSampleDataFloat(ArrayList<Float> sampleDataFloat) {
+		for(Float data : sampleDataFloat) {
+			System.out.println("Data: " + String.valueOf(data));
+		}
+	}
+	
+	private int mostDecimals (ArrayList<Float> sampleDataFloat) {
+		int mostDecimals = 0;
+		for(Float data : sampleDataFloat) {
+			String dataString = String.valueOf(data);
+			int pointLocation = dataString.indexOf('.');
+			int decimals = countDecimals(dataString, pointLocation);
+			
+			if(decimals > mostDecimals) {
+				mostDecimals = decimals;
+			}
+		}
+		return mostDecimals;
+	}
+	
+	private int countDecimals(String floatingString, int pointLocation) {
+		int decimals = (floatingString.length() - 1) - pointLocation;
+		if(decimals == 1) {
+			if(floatingString.charAt(pointLocation + 1) == '0') {
+				decimals = 0;
+			} 
+		}
+		return decimals;
+	}
+	
 	private int sturge(float range) {
 		return (int) Math.ceil(1 + (3.322 * Math.log10(range)));
 	}
 	
+	private float roundUp(float width, int mostDecimals) {
+		String widthString = String.valueOf(width);
+		int pointLocation = widthString.indexOf('.'); 
+		int decimals = countDecimals(widthString, pointLocation);
+		
+		if(decimals > mostDecimals) {
+			float roundUpCoefficient = coefficient(mostDecimals);
+			width += roundUpCoefficient;
+			widthString = String.valueOf(width);
+			widthString = removeExcessDecimals(widthString, mostDecimals);
+			width = Float.valueOf(widthString);
+		}
+		
+		return width;
+	}
+	
+	private float coefficient(int mostDecimals) {
+		float coefficient = 1;
+		
+		for(int i = 0 ; i < mostDecimals; i++) {
+			coefficient /= 10;
+		}
+		
+		return coefficient;
+	}
+	
+	private String removeExcessDecimals(String floatString, int mostDecimals) {
+		int pointLocation = floatString.indexOf('.');
+		floatString = floatString.substring(0, pointLocation + mostDecimals + 1);
+		
+		return floatString;
+	}
+	
 //	private ArrayList<String> classLimit(float lowestVal, int numClasses, float width) {
 //		ArrayList<String> classLimitList = new ArrayList<String>();
+//		ArrayList<Float> lowerClassLimitList = new ArrayList<Float>();
+//		ArrayList<Float> upperClassLimitList = new ArrayList<Float>();
 //		
 //		for(int i = 0; i < numClasses; i++) {
-//			
+//			lowerClassLimitList.add(lowestVal + (width * i));
 //		}
+//		
 //	}
 }
