3c059d3453c81ac9e202eaadcfba78fd4fdec3a0
Gamaliel Petralba <petralbagamaliel@gmail.com>
2017-03-24 17:38:53 +0800

    Collapse preliminaries.

--- Summarizing and Presenting Data/src/data_presentation/DataPresentationController.java
+++ Summarizing and Presenting Data/src/data_presentation/DataPresentationController.java
@@ -11,9 +11,10 @@ import javafx.fxml.Initializable;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Alert;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
 import javafx.scene.control.Alert.AlertType;
+import javafx.scene.control.Button;
+import javafx.scene.control.CheckBox;
+import javafx.scene.control.Label;
 import javafx.scene.layout.Pane;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
@@ -25,6 +26,7 @@ public class DataPresentationController implements Initializable {
 	@FXML private Button btnGenerateTable;
 	@FXML private Button btnGenerateGraph;
 	@FXML private Button btnFinish;
+	@FXML private CheckBox cbCollapse;
 	@FXML private Label lblTitle;
 	@FXML private Pane paneTable;
 
